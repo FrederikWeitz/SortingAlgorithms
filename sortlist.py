caf4616c393268cca4becd7feb0de449e2b57326
Frederik Weitz <frederik.weitz@online.de>
2018-08-10 18:48:14 +0200

    implemented bubble sort algorithm

--- sortlist.py
+++ sortlist.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import random
 
 class valueLine():
 
@@ -47,17 +48,64 @@ class valueLine():
         self.line['fill'] = 'white'
 
     def move(self, pos):
-        self.x_pos = pos
+        if type(pos)==int:
+            self.x_pos = pos
+        elif type(pos)==list:
+            self.x_pos = pos[0]
+        else:
+            return
         self.can.coords(self.line, self.x_limit + self.x_pos*self.x_step, self.y_limit,
                                          self.x_limit + self.x_pos*self.x_step, self.y_limit - self.value)
     
 
 class linelist():
 
-    def __init__(self):
+    def __init__(self, can):
+        self.can = can
         self.line = []
         self.maxValue=100
         self.minValue=0
         self.maxLine=20
 
-        
+    def setMaxValue(self, max):
+        self.maxValue = max
+
+    def getMaxValue(self):
+        return self.maxValue
+
+    def setMinValue(self, min):
+        self.minValue = min
+
+    def getMinValue(self):
+        return self.minValue
+
+    def setMaxLine(self, max):
+        self.maxLine = max
+
+    def getMaxLine(self):
+        return self.maxLine
+
+    def createList(self):
+        for i in range(self.maxLine):
+            self.line.append(valueLine(self.can))
+            self.line[i].setValue(random.randint(self.minValue,self.maxValue))
+            self.line[i].setx_pos(i)
+            self.line[i].setx_step(5)
+            self.line[i].setx_limit(20)
+            self.line[i].sety_limit(250)
+            self.line[i].createLine()
+
+    def deleteList(self):
+        for i in range(len(self.line)):
+            delEl = self.line.pop()
+            del delEl
+
+    def getLineValue(self, pos):
+        return self.line[pos].getValue()
+
+    def swapEls(self, pos1, pos2):
+        tempSwap = self.line[pos1]
+        self.line[pos1] = self.line[pos2]
+        self.line[pos2] = tempSwap
+        self.line[pos1].move(pos1)
+        self.line[pos2].move(pos2)
--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 from tkinter import *
-import random
 
-from sortlist import valueLine
+from sortlist import linelist
+from bubble import bubble
 
 root = Tk()
 
@@ -9,12 +9,12 @@ can = Canvas(root, height=400, width=600, bg='#000000')
 
 can.pack(expand=True)
 
-line = valueLine(can)
-line.setValue(100)
-line.setx_pos(0)
-line.setx_step(5)
-line.setx_limit(20)
-line.sety_limit(250)
-line.createLine()
+l = linelist(can)
+l.setMaxLine(100)
+l.createList()
+
+b = bubble(can)
+b.setList(l)
+b.sort()
 
 root.mainloop()
